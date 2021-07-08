99d3b1dab71bc640b034659d37ee2e59ab7b689e
Vinicius Vicentini <viniciusvicentini@live.com>
2021-07-08 20:20:01 -0300

    add storybook to project

---
+++ packages/button/stories/button.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Button from '../src'
+
+export default {
+  title: 'Button'
+}
+
+export const Default = () => (
+  <>
+    <Button bg="#fb62f6">Button</Button>
+  </>
+)
